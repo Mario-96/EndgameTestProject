a993d52b9895de5d3b572c6d798ae3a5f27ebe8d
Amaar Ahmed <amaar96@hotmail.co.uk>
2019-05-13 13:12:59 +0100

    Beginning of Project Github

---
+++ Asteroid Project/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    [SerializeField] Mesh[] asteroidModels;
+    //This represents the selection of models we have to use for our asteroids
+
+    [SerializeField] GameObject brokenAsteroidPrefab;
+
+    [SerializeField] int maxSpawns;
+    [SerializeField] int minSpawns;
+    //These variables are our min and max number of spawns when currently existing asteroids are destroyed
+
+    //Asteroid damage variable
+    [SerializeField] int asteroidDamage;
+
+
+    private void Start()
+    {
+        //Change mesh to random from asteroid model array
+        int modelIndex = Random.Range(0, asteroidModels.Length);
+        //modelIndex retrieves a number from our range
+
+        GetComponent<MeshFilter>().mesh = asteroidModels[modelIndex];
+        //Then the mesh is changed to the model number selected
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+
+        //Check if other is projectile or player
+        if (other.gameObject.tag == ("Spaceship")||other.gameObject.tag == ("Projectile"))
+        {
+            Debug.Log("Hit Detected");
+            //Play particles
+            //need to add particle effects to the game
+
+            //Generate random number to spawn
+            int asteroidsToSpawn = Random.Range(minSpawns, maxSpawns);
+            Debug.Log(asteroidsToSpawn);
+
+            float force = Random.Range(1, 3);
+
+            //Loop through asteroids to spawn count
+            for (int i = 0; i < asteroidsToSpawn; i++)
+            {
+                Vector3 originalPosition = other.transform.position;
+                //Spawn smaller asteriods
+                GameObject newAsteroid = Instantiate(brokenAsteroidPrefab, originalPosition, Quaternion.identity);
+
+                //Add force to smaller asteroids
+                Rigidbody rb = newAsteroid.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(force, transform.position, 5);
+                }
+            }
+
+            //If other is player deal damage
+
+            //Destroy this asteroid
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Asteroid Project/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody charRigidBody;
+    //This is where character controller is stored which will then be attached to the object
+
+    //[SerializeField] float jumpSpeed = 20.0f;
+    [SerializeField] float gravity = 1.0f;
+    float yVelocity = 0.0f;
+    //These are the variables for the our jumping movement
+
+    [SerializeField] float movespeed = 5.0f;
+    public float h;
+    public float v;
+    //These are the variables for our movement on the ground
+
+    // Use this for initialization
+    void Start()
+    {
+        charRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame, this checks for player input
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        //Here we get the values of H and V, and save them using the Input class
+
+        Vector3 direction = new Vector3(h, 0, v);
+        Vector3 velocity = direction * movespeed;
+
+        charRigidBody.position += velocity * Time.deltaTime;
+    }
+
+
+}
